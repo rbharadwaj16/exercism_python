6cc92dc5225142fe2e6776ae681d517bf620bb83
Raghavendra Bharadwaj <rbharadwaj16@gmail.com>
2025-11-11 23:17:57 +0530

    adding final bob.py (#21)

--- conditionals/bob.py
+++ conditionals/bob.py
@@ -1,12 +1,45 @@
-hey_bob = "UPPERCASE LETTERS"
+hey_bob = "UPPERCASE LETTERS ?"
 
 
-def is_yell(hey_bob):
-    for chars in hey_bob:
-        if (chars.isalpha() and chars.isupper()):
-            print(True)
-        else:
-            print(False)
+def is_question(hey_bob):
+    if hey_bob.strip().endswith("?"):
+        return True
+    else:
+        return False
+    
+def is_capital(hey_bob):
+    # Check if the string has letters and all letters are uppercase
+    if hey_bob.isupper() and any(c.isalpha() for c in hey_bob):
+        return True
+    else:
+        return False
 
+def is_question_and_capital(hey_bob):
+    if is_question(hey_bob) and is_capital(hey_bob):
+        return True
+    else:
+        return False
+    
 
-is_yell(hey_bob)
+def is_silence(hey_bob):
+    if hey_bob == "":
+        return True
+    elif hey_bob.isspace():
+        return True
+    else:
+        return False
+    
+
+def response(hey_bob):
+    if is_silence(hey_bob) == True:
+        return "Fine. Be that way!"
+    elif (is_question(hey_bob) == True) and (is_capital(hey_bob) == True):
+        return "Calm down, I know what I'm doing!"
+    elif is_question(hey_bob) == True:
+        return "Sure."
+    elif is_capital(hey_bob) == True:
+        return "Whoa, chill out!"
+    else:
+        return "Whatever."
+
+print(response(hey_bob))
