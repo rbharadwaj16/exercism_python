90500e1988ce375d43de5c08af1162bc20e9cf76
rbharadwaj16 <rbharadwaj16@gmail.com>
2025-11-13 22:43:30 +0530

    completed isogram.py

--- strings/isogram.py
+++ strings/isogram.py
@@ -1,19 +1,3 @@
-x = "isogram"
-
-
-def is_isogram(x):
-    for i in x:
-        if x.count(i) > 1:
-            print(i) 
-
-
-# def is_isogram(x):
-#     for i in x:
-#         if x.find(i) == -1:
-#             return True
-#         else:
-#             return False
-        
-
-
-print(is_isogram(x))
+def is_isogram(word):
+    word = word.lower().replace(" ","").replace("-","")
+    return len(word) == len(set(word))
