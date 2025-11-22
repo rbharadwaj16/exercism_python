9995a44451558e7accb327fe06df9699e2851c4e
rbharadwaj16 <rbharadwaj16@gmail.com>
2025-11-22 23:38:49 +0530

    wasn't clear

---
+++ lists/resist_colors.py
@@ -0,0 +1,16 @@
+
+
+
+def color_code(color):
+    colors = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"]
+    
+    for index, value in enumerate(colors):
+        if value == color:
+            return index
+    
+
+
+def colors():
+    colors = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"]
+
+    return colors
