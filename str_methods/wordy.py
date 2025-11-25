e399688e2cab3ac28ab841ecbbda3727425f4216
rbharadwaj16 <rbharadwaj16@gmail.com>
2025-11-25 23:04:43 +0530

    adding final wordy

--- str_methods/wordy.py
+++ str_methods/wordy.py
@@ -1,9 +1,33 @@
-question = "What is 50 divided by 2?"
+    if not question.startswith("What is") or "cubed" in question:
+        raise ValueError("unknown operation")
+    
+    question = question.removeprefix("What is")
+    question = question.removesuffix("?")
+    question = question.replace("by", "")
+    question = question.strip()
 
-question = question.removeprefix("What is")
-question = question.removesuffix("?")
-question = question.replace("by", "")
-question = question.strip()
-question = question.split()
+    if not question:
+        raise ValueError("syntax error")
 
-print(question)
+    formula = question.split()
+    while len(formula) > 1:
+        try:
+            x_value = int(formula[0])
+            y_value = int(formula[2])
+            symbol = formula[1]
+            remainder = formula[3:]
+
+            if symbol == "plus":
+                formula = [x_value + y_value] + remainder
+            elif symbol == "minus":
+                formula = [x_value - y_value] + remainder
+            elif symbol == "multiplied":
+                formula = [x_value * y_value] + remainder
+            elif symbol == "divided":
+                formula = [x_value / y_value] + remainder
+            else:
+                raise ValueError("syntax error")
+        except:
+            raise ValueError("syntax error")
+
+    return int(formula[0])
