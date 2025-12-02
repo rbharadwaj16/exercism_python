cb572b3b7bb978409635747d2bfbd509120c1966
Raghavendra Bharadwaj <rbharadwaj16@gmail.com>
2025-12-02 22:30:41 +0530

    Loops/binary search (#52)
    * fixing valueerror
    
    * fixed ValueError

---
+++ loops/binary_search.py
@@ -0,0 +1,25 @@
+search_list = [4, 8, 12, 14, 20]
+value = 20
+
+sorted_list = sorted(search_list)
+
+
+def binary_search(sorted_list, value):
+    lowest_index = 0
+    highest_index = len(sorted_list) - 1
+
+    while lowest_index <= highest_index:
+        middle_index = (lowest_index + highest_index) // 2
+        if sorted_list[middle_index] == value:
+            return middle_index
+        elif sorted_list[middle_index] > value:
+            highest_index = middle_index - 1
+        else:
+            sorted_list[middle_index] < value
+            lowest_index = middle_index + 1
+
+    raise ValueError("value not in array")
+        
+
+print(binary_search(sorted_list, value))
+
