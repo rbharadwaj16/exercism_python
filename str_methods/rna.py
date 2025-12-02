b775521c92c3b7b4006c0ee6a9ebcd49a08fd463
Raghavendra Bharadwaj <rbharadwaj16@gmail.com>
2025-12-02 15:59:26 +0530

    Str methods/rna (#49)
    * initial commit
    
    * adding maketrans
    
    * creating translate wrap
    
    * return rna_strand

---
+++ str_methods/rna.py
@@ -0,0 +1,4 @@
+def to_rna(dna_strand):
+    replacements = dna_strand.maketrans({"G": "C", "C": "G", "T": "A", "A": "U"})
+    rna_strand = dna_strand.translate(replacements)
+    return rna_strand
