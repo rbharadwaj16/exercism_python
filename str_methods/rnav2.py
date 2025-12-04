0dda9fcabe724720ab3ca8b70d417b3df9334cb6
Raghavendra Bharadwaj <rbharadwaj16@gmail.com>
2025-12-04 16:00:36 +0530

    adding rna second iteration per review feedback (#58)

---
+++ str_methods/rnav2.py
@@ -0,0 +1,6 @@
+def to_rna(dna_strand):
+    #replacements = dna_strand.maketrans({"G": "C", "C": "G", "T": "A", "A": "U"})
+    #return dna_strand.translate(replacements)
+    #Incorporating code changes per the review
+    replacements = dna_strand.maketrans('GCTA','CGAU')
+    return dna_strand.translate(replacements)
