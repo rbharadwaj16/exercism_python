c35579cb3b0ffa6c7af1f6e6fd9ae809e715148a
Raghavendra Bharadwaj <rbharadwaj16@gmail.com>
2025-12-13 16:50:53 +0530

    wrapped up anagram (#74)

--- list_methods/anagram.py
+++ list_methods/anagram.py
@@ -1,3 +1,12 @@
-def find_anagrams(word, candidates):
-    if word in candidates:
-        return 
+
+word = "diaper"
+candidates = ["hello", "world", "zombies", "pants"]
+
+result = []
+
+for candidate in candidates:
+    if len(word) == len(candidate) and candidate.lower() != word.lower():
+        if sorted(word.lower()) == sorted(candidate.lower()):
+            result.append(candidate)
+
+print(result)
