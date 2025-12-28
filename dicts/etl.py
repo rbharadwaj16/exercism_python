daa446beb24c4db068417e6ac24e3ceba5b7056a
Raghavendra Bharadwaj <rbharadwaj16@gmail.com>
2025-12-28 17:01:41 +0530

    let's roll.. resume back with dicts (#89)

--- dicts/etl.py
+++ dicts/etl.py
@@ -1,2 +1,12 @@
-def transform(legacy_data):
-    pass
+legacy_data = {1: ["A"]}
+
+
+
+base_dict = dict()
+ 
+for i in legacy_data:
+    for item in legacy_data[i]:
+        base_dict[item.lower()] = i
+
+
+print(base_dict)
