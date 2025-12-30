d41b4e21dd06a2cffcfbc462938ab9fcb1995109
Raghavendra Bharadwaj <rbharadwaj16@gmail.com>
2025-12-30 16:44:55 +0530

    adding task 2 dict methods (#93)

--- dict_methods/mecha_munch_mgmt.py
+++ dict_methods/mecha_munch_mgmt.py
@@ -1,13 +1,11 @@
 # current_cart = {'Banana': 3, 'Apple': 2, 'Orange': 1}
 # items = ('Apple', 'Apple', 'Orange', 'Apple', 'Banana')
 
-current_cart = {"Apple": 1, "Banana": 4}
+#current_cart = {"Apple": 1, "Banana": 4}
 items = ("Apple", "Banana", "Orange")
+new_cart = {}
 
 for i in items:
-    if i in current_cart:
-        current_cart[i] += 1
-    else:
-        current_cart.setdefault(i, 1)
+    new_cart.setdefault(i, 1)
 
-print (current_cart)
+print(new_cart)
