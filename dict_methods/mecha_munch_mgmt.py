1bd501d24b287ec402a3400fe0c8443f77b21617
Raghavendra Bharadwaj <rbharadwaj16@gmail.com>
2025-12-30 23:12:03 +0530

    task 3 dict methods (#94)

--- dict_methods/mecha_munch_mgmt.py
+++ dict_methods/mecha_munch_mgmt.py
@@ -1,11 +1,10 @@
-# current_cart = {'Banana': 3, 'Apple': 2, 'Orange': 1}
-# items = ('Apple', 'Apple', 'Orange', 'Apple', 'Banana')
-
-#current_cart = {"Apple": 1, "Banana": 4}
-items = ("Apple", "Banana", "Orange")
-new_cart = {}
-
-for i in items:
-    new_cart.setdefault(i, 1)
-
-print(new_cart)
+def update_recipes(ideas, update):
+    """Update recipes with new ingredient quantities.
+    
+    :param ideas: dict - recipe names and their ingredient dictionaries
+    :param update: tuple - tuples of (recipe_name, updated_ingredients_dict)
+    :return: dict - updated recipe dictionary
+    """
+    for recipe_name, updated_ingredients in update:
+        ideas[recipe_name] = updated_ingredients
+    return ideas
