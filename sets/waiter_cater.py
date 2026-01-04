f10561d15d65f076e4286c97ea6ae695d2331418
rbharadwaj16 <rbharadwaj16@gmail.com>
2026-01-04 18:09:52 +0530

    adding task 4

--- sets/waiter_cater.py
+++ sets/waiter_cater.py
@@ -1,124 +1,26 @@
-"""Functions for compiling dishes and ingredients for a catering company."""
+dish = ('Ginger Glazed Tofu Cutlets', ['tofu', 'soy sauce', 'ginger', 'corn starch', 'garlic', 'brown sugar', 'sesame seeds', 'lemon juice'])
 
 
-from sets_categories_data import (VEGAN,
-                                  VEGETARIAN,
-                                  KETO,
-                                  PALEO,
-                                  OMNIVORE,
-                                  ALCOHOLS,
-                                  SPECIAL_INGREDIENTS)
+SPECIAL_INGREDIENTS = {'cream','bacon', 'garlic', 'baby scallops', 'mussels', 'baby squid', 'cashews', 'salmon fillets',
+                       'filo pastry', 'almonds', 'milk', 'blue cheese', 'clams', 'shrimp', 'tomato puree', 'chocolate',
+                       'honey', 'anchovy fillets', 'bulgur', 'prawns', 'parmesan cheese', 'fish', 'shelled large shrimp',
+                       'gluten', 'crab legs', 'feta cheese', 'whole-milk yogurt', 'crema', 'firm tofu', 'fish stock',
+                       'fresh ricotta', 'tomato paste', 'fresh cherry tomatoes', 'pork chops', 'eggs', 'greek yogurt',
+                       'hazelnuts', 'pecans', 'brie cheese', 'oaxaca cheese', 'yellow onion', 'whey', 'silken tofu',
+                       'toasted bread', 'parmesan', 'beef', 'tofu', 'flour', 'tomatoes', 'red onion', 'slivered almonds',
+                       'strawberries', 'onions', 'pine nuts', 'cherry tomatoes', 'soy sauce', 'oyster sauce',
+                       'mozzarella cheese', 'roma tomatoes', 'heavy cream', 'paneer', 'pork tenderloin', 'garlic cloves',
+                       'swiss cheese', 'grilled king fish', 'ground almonds', 'tilapia', 'sprint onion', 'couscous',
+                       'walnuts', 'semolina', 'yogurt', 'cotija cheese', 'oysters', 'spaghetti', 'cheddar cheese',
+                       'butter', 'lobster', 'smoked tofu', 'peanuts', 'ground pork', 'fresh cherry bocconcini',
+                       'pork belly', 'toasted peanuts', 'roasted peanuts'
+                       }
 
 
-def clean_ingredients(dish_name, dish_ingredients):
-    """Remove duplicates from `dish_ingredients`.
+new_dish = set(dish[1])
 
-    :param dish_name: str - containing the dish name.
-    :param dish_ingredients: list - dish ingredients.
-    :return: tuple - containing (dish_name, ingredient set).
+common = new_dish.intersection(SPECIAL_INGREDIENTS)
+final = (dish[0], common)
 
-    This function should return a `tuple` with the name of the dish as the first item,
-    followed by the de-duped `set` of ingredients as the second item.
-    """
+print(type(final))
 
-    return ((dish_name, set(dish_ingredients)))
-
-
-def check_drinks(drink_name, drink_ingredients):
-    """Append "Cocktail" (alcohol)  or "Mocktail" (no alcohol) to `drink_name`, based on `drink_ingredients`.
-
-    :param drink_name: str - name of the drink.
-    :param drink_ingredients: list - ingredients in the drink.
-    :return: str - drink_name appended with "Mocktail" or "Cocktail".
-
-    The function should return the name of the drink followed by "Mocktail" (non-alcoholic) and drink
-    name followed by "Cocktail" (includes alcohol).
-
-    """
-
-    if ALCOHOLS.isdisjoint(drink_ingredients):
-        return (f'{drink_name} Mocktail')
-    else:
-        return (f'{drink_name} Cocktail')
-
-
-def categorize_dish(dish_name, dish_ingredients):
-    """Categorize `dish_name` based on `dish_ingredients`.
-
-    :param dish_name: str - dish to be categorized.
-    :param dish_ingredients: set - ingredients for the dish.
-    :return: str - the dish name appended with ": <CATEGORY>".
-
-    This function should return a string with the `dish name: <CATEGORY>` (which meal category the dish belongs to).
-    `<CATEGORY>` can be any one of  (VEGAN, VEGETARIAN, PALEO, KETO, or OMNIVORE).
-    All dishes will "fit" into one of the categories imported from `sets_categories_data.py`
-
-    """
-    if dish_ingredients.issubset(VEGAN):
-        return (f'{dish_name}: VEGAN')
-    elif dish_ingredients.issubset(VEGETARIAN):
-        return (f'{dish_name}: VEGETARIAN')
-    elif dish_ingredients.issubset(PALEO):
-        return (f'{dish_name}: PALEO')
-    elif dish_ingredients.issubset(KETO):
-        return (f'{dish_name}: KETO')
-    elif dish_ingredients.issubset(OMNIVORE):
-        return (f'{dish_name}: OMNIVORE')
-
-
-def tag_special_ingredients(dish):
-    """Compare `dish` ingredients to `SPECIAL_INGREDIENTS`.
-
-    :param dish: tuple - of (dish name, list of dish ingredients).
-    :return: tuple - containing (dish name, dish special ingredients).
-
-    Return the dish name followed by the `set` of ingredients that require a special note on the dish description.
-    For the purposes of this exercise, all allergens or special ingredients that need to be tracked are in the
-    SPECIAL_INGREDIENTS constant imported from `sets_categories_data.py`.
-    """
-
-    pass
-
-
-def compile_ingredients(dishes):
-    """Create a master list of ingredients.
-
-    :param dishes: list - of dish ingredient sets.
-    :return: set - of ingredients compiled from `dishes`.
-
-    This function should return a `set` of all ingredients from all listed dishes.
-    """
-
-    pass
-
-
-def separate_appetizers(dishes, appetizers):
-    """Determine which `dishes` are designated `appetizers` and remove them.
-
-    :param dishes: list - of dish names.
-    :param appetizers: list - of appetizer names.
-    :return: list - of dish names that do not appear on appetizer list.
-
-    The function should return the list of dish names with appetizer names removed.
-    Either list could contain duplicates and may require de-duping.
-    """
-
-    pass
-
-
-def singleton_ingredients(dishes, intersection):
-    """Determine which `dishes` have a singleton ingredient (an ingredient that only appears once across dishes).
-
-    :param dishes: list - of ingredient sets.
-    :param intersection: constant - can be one of `<CATEGORY>_INTERSECTIONS` constants imported from `sets_categories_data.py`.
-    :return: set - containing singleton ingredients.
-
-    Each dish is represented by a `set` of its ingredients.
-
-    Each `<CATEGORY>_INTERSECTIONS` is an `intersection` of all dishes in the category. `<CATEGORY>` can be any one of:
-        (VEGAN, VEGETARIAN, PALEO, KETO, or OMNIVORE).
-
-    The function should return a `set` of ingredients that only appear in a single dish.
-    """
-
-    pass
