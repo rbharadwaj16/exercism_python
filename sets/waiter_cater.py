ebe700a71dbc69b1cd907ae45c95b3801eee2cf1
rbharadwaj16 <rbharadwaj16@gmail.com>
2026-01-04 19:53:28 +0530

    task 5 - done

--- sets/waiter_cater.py
+++ sets/waiter_cater.py
@@ -1,26 +1,15 @@
-dish = ('Ginger Glazed Tofu Cutlets', ['tofu', 'soy sauce', 'ginger', 'corn starch', 'garlic', 'brown sugar', 'sesame seeds', 'lemon juice'])
+dishes =    ['Avocado Deviled Eggs','Flank Steak with Chimichurri and Asparagus', 'Kingfish Lettuce Cups',
+             'Grilled Flank Steak with Caesar Salad','Vegetarian Khoresh Bademjan','Avocado Deviled Eggs',
+             'Barley Risotto','Kingfish Lettuce Cups']
+
+appetizers = ['Kingfish Lettuce Cups','Avocado Deviled Eggs','Satay Steak Skewers',
+              'Dahi Puri with Black Chickpeas','Avocado Deviled Eggs','Asparagus Puffs',
+              'Asparagus Puffs']
 
 
-SPECIAL_INGREDIENTS = {'cream','bacon', 'garlic', 'baby scallops', 'mussels', 'baby squid', 'cashews', 'salmon fillets',
-                       'filo pastry', 'almonds', 'milk', 'blue cheese', 'clams', 'shrimp', 'tomato puree', 'chocolate',
-                       'honey', 'anchovy fillets', 'bulgur', 'prawns', 'parmesan cheese', 'fish', 'shelled large shrimp',
-                       'gluten', 'crab legs', 'feta cheese', 'whole-milk yogurt', 'crema', 'firm tofu', 'fish stock',
-                       'fresh ricotta', 'tomato paste', 'fresh cherry tomatoes', 'pork chops', 'eggs', 'greek yogurt',
-                       'hazelnuts', 'pecans', 'brie cheese', 'oaxaca cheese', 'yellow onion', 'whey', 'silken tofu',
-                       'toasted bread', 'parmesan', 'beef', 'tofu', 'flour', 'tomatoes', 'red onion', 'slivered almonds',
-                       'strawberries', 'onions', 'pine nuts', 'cherry tomatoes', 'soy sauce', 'oyster sauce',
-                       'mozzarella cheese', 'roma tomatoes', 'heavy cream', 'paneer', 'pork tenderloin', 'garlic cloves',
-                       'swiss cheese', 'grilled king fish', 'ground almonds', 'tilapia', 'sprint onion', 'couscous',
-                       'walnuts', 'semolina', 'yogurt', 'cotija cheese', 'oysters', 'spaghetti', 'cheddar cheese',
-                       'butter', 'lobster', 'smoked tofu', 'peanuts', 'ground pork', 'fresh cherry bocconcini',
-                       'pork belly', 'toasted peanuts', 'roasted peanuts'
-                       }
+dishes_set = set(dishes)
+appetizers_set = set(appetizers)
 
+diff = dishes_set.difference(appetizers_set)
 
-new_dish = set(dish[1])
-
-common = new_dish.intersection(SPECIAL_INGREDIENTS)
-final = (dish[0], common)
-
-print(type(final))
-
+print(diff)
