45ae18c21f594f565f11b895eaef8368f3811ffa
Raghavendra Bharadwaj <rbharadwaj16@gmail.com>
2026-01-06 16:27:07 +0530

    adding list of wagons (#105)

--- unpack_and_pack/loco_engineer.py
+++ unpack_and_pack/loco_engineer.py
@@ -17,33 +17,12 @@ def fix_list_of_wagons(each_wagons_id, missing_wagons):
     :param missing_wagons: list - the list of missing wagons.
     :return: list - list of wagons.
     """
-    pass
+    a, b, c, *last = each_wagons_id
+    
+    combined = [c, *missing_wagons, *last, a, b]
+    
+    return (combined)
 
 
-def add_missing_stops():
-    """Add missing stops to route dict.
-
-    :param route: dict - the dict of routing information.
-    :param: arbitrary number of stops.
-    :return: dict - updated route dictionary.
-    """
-    pass
 
 
-def extend_route_information(route, more_route_information):
-    """Extend route information with more_route_information.
-
-    :param route: dict - the route information.
-    :param more_route_information: dict -  extra route information.
-    :return: dict - extended route information.
-    """
-    pass
-
-
-def fix_wagon_depot(wagons_rows):
-    """Fix the list of rows of wagons.
-
-    :param wagons_rows: list[list[tuple]] - the list of rows of wagons.
-    :return: list[list[tuple]] - list of rows of wagons.
-    """
-    pass
