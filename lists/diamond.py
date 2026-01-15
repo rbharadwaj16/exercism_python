71031e224ee74fa48709447e127a9ae85a6e5b0a
Raghavendra Bharadwaj <rbharadwaj16@gmail.com>
2026-01-15 15:41:47 +0530

    diamond kata pass test cases (#115)

--- lists/diamond.py
+++ lists/diamond.py
@@ -1,21 +1,26 @@
-alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-target = "D"
+ALPHABETS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+letter = "D"
 
-max_idx = alphabet.find(target)
 
-rows = []
-for i in range(max_idx + 1):
-    letter = alphabet[i]
-    outer_spaces = " " * (max_idx - i)
+n = ALPHABETS.index(letter)
+
+
+top = []
+
+for i in range(n + 1):
+    ch = ALPHABETS[i]
+    outer = " " * (n - i)
+
     if i == 0:
-        rows.append(outer_spaces + letter + outer_spaces)
+        line = outer + ch + outer
     else:
-        inner_spaces = " " * (2 * i - 1)
-        rows.append(outer_spaces + letter + inner_spaces + letter + outer_spaces)
+        inner = " " * (2*i - 1)
+        line = outer + ch + inner + ch + outer
 
-# Mirror the rows (excluding the middle row)
-for i in range(max_idx - 1, -1, -1):
-    rows.append(rows[i])
+    top.append(line)
 
-for row in rows:
-    print(f'"{row}",')
+bottom = top[-2::-1]
+
+
+
+print(top + bottom)
