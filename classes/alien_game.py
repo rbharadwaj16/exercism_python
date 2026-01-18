97920e5eb2c0a445b00aa09e57e6b17b1e58b67c
rbharadwaj16 <rbharadwaj16@gmail.com>
2026-01-18 16:09:40 +0530

    alien game functions

--- classes/alien_game.py
+++ classes/alien_game.py
@@ -16,16 +16,34 @@ class Alien:
     teleport(new_x_coordinate, new_y_coordinate): Move Alien object to new coordinates.
     collision_detection(other): Implementation TBD.
     """
-
+    total_aliens_created = 0
     def __init__ (self, x_coordinate, y_coordinate):
             self.x_coordinate = x_coordinate
             self.y_coordinate = y_coordinate
             self.health = 3
+            Alien.total_aliens_created += 1
+        
+    def hit(self):
+        self.health = self.health - 1
 
+    def is_alive(self):
+        if self.health > 0:
+            return True
+        else:
+            return False
+            
+    def teleport(self, new_x, new_y):
+        self.x_coordinate = new_x
+        self.y_coordinate = new_y
+
+    def collision_detection(self, test):
+        pass
+        
+        
 
 #TODO:  create the new_aliens_collection() function below to call your Alien class with a list of coordinates.
 
 
-def new_aliens_collection(x,y):
-    return Alien(x, y)
+def new_aliens_collection(positions):
+    return [Alien(x,y) for x,y in positions]
     
