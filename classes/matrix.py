949acadae82dd9e2de5859cd367ad251d38e7840
rbharadwaj16 <rbharadwaj16@gmail.com>
2026-02-02 23:10:46 +0530

    getting back to rythm after baby's birth! let's do it.

--- classes/matrix.py
+++ classes/matrix.py
@@ -1,9 +1,14 @@
 class Matrix:
     def __init__(self, matrix_string):
-        pass
-
+        rows = matrix_string.split("\n")
+        self.matrix = []
+        for r in rows:
+            row_elements = r.split(" ")
+            self.matrix.append([int(element) for element in row_elements])
     def row(self, index):
-        pass
-
+        return self.matrix[index-1]
     def column(self, index):
-        pass
+        columns = []
+        for i in range(len(self.matrix)):
+            columns.append(self.matrix[i][index-1])
+        return columns
