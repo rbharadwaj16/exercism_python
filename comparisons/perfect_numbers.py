8f7314cba0f081366278dc66da6c0b7c04b49e57
rbharadwaj16 <rbharadwaj16@gmail.com>
2026-02-11 23:45:01 +0530

    perfect numbers all UC passed

--- comparisons/perfect_numbers.py
+++ comparisons/perfect_numbers.py
@@ -1,9 +1,24 @@
-number = 6
+
+number = 2
 processing_list = []
 
+if number < 1:
+    raise ValueError("Classification is only possible for positive integers.")
 
-for i in range(number):
-    if (int(i/number) == 0):
-        processing_list.append(i)
+else:
+    for i in range(number):
+        if i !=0 and (int(number % i) == 0):
+            processing_list.append(i)
 
-print(processing_list)
+    final_sum = sum(processing_list)
+
+    if final_sum == number:
+        print("perfect")
+    elif number < final_sum:
+        print("abundant")
+    else:
+        print("deficient")
+        
+
+
+
